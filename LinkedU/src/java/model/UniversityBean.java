11d56fe0386860fa78989bfcaf5f720b35b8e471
jdfost1 <IT353S833@AD.ILSTU.EDU>
2018-04-13 23:10:31 -0500

    added university sign up page and link on index to sign up

--- LinkedU/src/java/model/UniversityBean.java
+++ LinkedU/src/java/model/UniversityBean.java
@@ -12,14 +12,101 @@ import javax.faces.bean.SessionScoped;
  *
  * @author IT353S833
  */
-@ManagedBean
+@ManagedBean(name="UniversityBean")
 @SessionScoped
 public class UniversityBean {
+
+    String collegeName;
+    String city;
+    String state;
+    String address;
+    String phone;
+    String tuition;
+    String enrollment;
+    String mascot;
+    String dean;
 
     /**
      * Creates a new instance of UniversityBean
      */
     public UniversityBean() {
     }
+    public void createUniversity(){
+        
+    //add university to data base
     
+    }//end of create university
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTuition() {
+        return tuition;
+    }
+
+    public void setTuition(String tuition) {
+        this.tuition = tuition;
+    }
+
+    public String getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(String enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getMascot() {
+        return mascot;
+    }
+
+    public void setMascot(String mascot) {
+        this.mascot = mascot;
+    }
+
+    public String getDean() {
+        return dean;
+    }
+
+    public void setDean(String dean) {
+        this.dean = dean;
+    }
+
 }
